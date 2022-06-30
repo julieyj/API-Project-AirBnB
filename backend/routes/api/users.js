116a904a6138a1407d6307391a2e925f2fcb3aef
julieyj <julieyjjung@gmail.com>
2022-06-29 22:07:48 -0700

    phase 4 user signup api route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,42 @@
 const express = require("express");
+
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
 const router = express.Router();
 
+// Sign up
+router.post(
+  '/',
+  async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user
+    });
+  }
+);
+
+
+
+
 module.exports = router;
+
+
+// fetch("/api/users", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": `Mxxvj2YW-rSX-J-2ig-aPVdFt8eUo8BfQlHU`,
+//   },
+//   body: JSON.stringify({
+//     email: "spidey@spider.man",
+//     username: "Spidey",
+//     password: "password",
+//   }),
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
