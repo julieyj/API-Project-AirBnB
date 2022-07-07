705c090275f6e0fa6b79f880a748c2322e026a17
julieyj <julieyjjung@gmail.com>
2022-07-07 14:02:07 -0700

    remove /api

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const apiRouter = require("./api");
-router.use("/api", apiRouter);
+router.use("/", apiRouter);
 
 // Test Route
 // router.get("/hello/world", function (req, res) {
@@ -11,7 +11,7 @@ router.use("/api", apiRouter);
 // });
 
 // Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
+router.get("/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
   res.status(200).json({
