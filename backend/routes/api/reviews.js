09b687489d7c1e0acbf9157701ce1e588c329cb4
julieyj <julieyjjung@gmail.com>
2022-07-08 11:10:42 -0700

    images error handler complete

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -199,30 +199,40 @@ router.delete("/:id", requireAuth, reviewUserAuth, async (req, res, next) => {
 });
 
 // Add an image to review based on review id
-router.post(
-  "/:id/images",
-  requireAuth,
-  reviewUserAuth,
-  async (req, res, next) => {
-    const { url } = req.body;
+router.post("/:id/images", requireAuth, reviewUserAuth, async (req, res, next) => {
+  const { url } = req.body;
 
-    const reviewImage = await Image.create({
+  const imageMaxCount = await Image.findAll({
+    where: {
       reviewId: req.params.id,
-      imageableType: "Review",
-      url: url,
-    });
-
-    if (!reviewImage) {
-      const err = new Error("Not found");
-      err.status = 404;
-      err.title = "Not found";
-      err.errors = ["Review couldn't be found"];
-      next(err);
+      imageableType: 'Review'
     }
+  });
 
-    return res.json({ reviewImage });
+  if (imageMaxCount.length >= 10) {
+    const err = new Error("Validation error");
+    err.status = 400;
+    err.title = "Validation error";
+    err.errors = ["Maximum number of images for this resource was reached"];
+    next(err);
   }
-);
+
+  const reviewImage = await Image.create({
+    reviewId: req.params.id,
+    imageableType: "Review",
+    url: url,
+  });
+
+  if (!reviewImage) {
+    const err = new Error("Not found");
+    err.status = 404;
+    err.title = "Not found";
+    err.errors = ["Review couldn't be found"];
+    next(err);
+  }
+
+  return res.json({ reviewImage });
+});
 
 // Get all reviews
 router.get("/", async (req, res) => {
