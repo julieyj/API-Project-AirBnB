9ff77ec1eb234a206fdf2be2734b0b302035a2c3
julieyj <julieyjjung@gmail.com>
2022-07-21 23:26:03 -0700

    prep for heroku

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -38,18 +38,18 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
   router.get('/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(201).json({})
   });
 }
 
 // Add a XSRF-TOKEN cookie
-router.get("/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
-  });
-});
+// router.get("/csrf/restore", (req, res) => {
+//   const csrfToken = req.csrfToken();
+//   res.cookie("XSRF-TOKEN", csrfToken);
+//   res.status(200).json({
+//     'XSRF-Token': csrfToken
+//   });
+// });
 
 
 module.exports = router;
