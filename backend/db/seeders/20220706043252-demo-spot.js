0c265ef46a75db226280b3669c646f4f6eb667bd
julieyj <julieyjjung@gmail.com>
2022-08-02 21:59:43 -0700

    seeder update

--- backend/db/seeders/20220706043252-demo-spot.js
+++ backend/db/seeders/20220706043252-demo-spot.js
@@ -37,7 +37,7 @@ module.exports = {
         },
         {
           userId: 3,
-          address: "951 Chicago AVe",
+          address: "951 Chicago Ave",
           city: "Oak Park",
           state: "Illinois",
           country: "USA",
@@ -97,7 +97,7 @@ module.exports = {
         },
         {
           userId: 3,
-          address: "951 Chicago AVe",
+          address: "951 Chicago Ave",
           city: "Oak Park",
           state: "Illinois",
           country: "USA",
@@ -157,127 +157,7 @@ module.exports = {
         },
         {
           userId: 3,
-          address: "951 Chicago AVe",
-          city: "Oak Park",
-          state: "Illinois",
-          country: "USA",
-          lat: 41.894279810000654,
-          lng: -87.79955252732101,
-          name: "Frank Lloyd Wright Home & Studio",
-          description:
-            "Frank Lloyd Wright's first peronsal home and studio is opening its doors for a limited time to select overnight guests - come and experience the birthplace of Wright's vision for a new American architecture.",
-          price: 5000,
-          previewImage:
-            "https://s3.amazonaws.com/architecture-org/files/modules/frank-lloyd-wright-home-and-studio-eric-allix-roge.jpg",
-        },
-        {
-          userId: 1,
-          address: "737 Frenchmans Road",
-          city: "Stanford",
-          state: "California",
-          country: "USA",
-          lat: 41.894279810000654,
-          lng: -87.79955252732101,
-          name: "The Hanna-Honeycomb House",
-          description:
-            "The long-term collaboration between the Hannas and Wright resulted in an unprecedented design: a house based on hexagonal geometry, with no right angles in the floor plan.",
-          price: 7000,
-          previewImage:
-            "https://franklloydwright.org/wp-content/uploads/2017/02/Hanna-House-exterior-1440x640.jpg",
-        },
-        {
-          userId: 1,
-          address: "4800 Hollywood Blvd",
-          city: "Los Angeles",
-          state: "California",
-          country: "USA",
-          lat: 34.10025929155542,
-          lng: -118.29453545969307,
-          name: "Hollyhock House",
-          description:
-            "The iconic Frank Lloyd Wright home is open for a limited time to overnight guests - come take in the views and architecture of LA's UNESCO World Heritage site!",
-          price: 3000,
-          previewImage:
-            "https://cdn.vox-cdn.com/thumbor/DOPbz_C-mTvI2JEN69UzVovm2tA=/0x0:3600x1712/920x613/filters:focal(1512x914:2088x1490):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/64687543/Hollyhock_House_ext_Photo_by_Joshua_White.0.jpg",
-        },
-        {
-          userId: 2,
-          address: "125 Jewett",
-          city: "Buffalo",
-          state: "New York",
-          country: "USA",
-          lat: 42.93627207354969,
-          lng: -78.84793099149458,
-          name: "Martin House",
-          description:
-            "One of Frank Lloyd Wright's most important works, this quintessential Prairie house is now accepting select overnight guests - come take in the views of the magnficent landscape and historic architecture!",
-          price: 8000,
-          previewImage:
-            "https://franklloydwright.org/wp-content/uploads/2021/01/MartinHouse024-1440x640.jpg",
-        },
-        {
-          userId: 3,
-          address: "951 Chicago AVe",
-          city: "Oak Park",
-          state: "Illinois",
-          country: "USA",
-          lat: 41.894279810000654,
-          lng: -87.79955252732101,
-          name: "Frank Lloyd Wright Home & Studio",
-          description:
-            "Frank Lloyd Wright's first peronsal home and studio is opening its doors for a limited time to select overnight guests - come and experience the birthplace of Wright's vision for a new American architecture.",
-          price: 5000,
-          previewImage:
-            "https://s3.amazonaws.com/architecture-org/files/modules/frank-lloyd-wright-home-and-studio-eric-allix-roge.jpg",
-        },
-        {
-          userId: 1,
-          address: "737 Frenchmans Road",
-          city: "Stanford",
-          state: "California",
-          country: "USA",
-          lat: 41.894279810000654,
-          lng: -87.79955252732101,
-          name: "The Hanna-Honeycomb House",
-          description:
-            "The long-term collaboration between the Hannas and Wright resulted in an unprecedented design: a house based on hexagonal geometry, with no right angles in the floor plan.",
-          price: 7000,
-          previewImage:
-            "https://franklloydwright.org/wp-content/uploads/2017/02/Hanna-House-exterior-1440x640.jpg",
-        },
-        {
-          userId: 1,
-          address: "4800 Hollywood Blvd",
-          city: "Los Angeles",
-          state: "California",
-          country: "USA",
-          lat: 34.10025929155542,
-          lng: -118.29453545969307,
-          name: "Hollyhock House",
-          description:
-            "The iconic Frank Lloyd Wright home is open for a limited time to overnight guests - come take in the views and architecture of LA's UNESCO World Heritage site!",
-          price: 3000,
-          previewImage:
-            "https://cdn.vox-cdn.com/thumbor/DOPbz_C-mTvI2JEN69UzVovm2tA=/0x0:3600x1712/920x613/filters:focal(1512x914:2088x1490):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/64687543/Hollyhock_House_ext_Photo_by_Joshua_White.0.jpg",
-        },
-        {
-          userId: 2,
-          address: "125 Jewett",
-          city: "Buffalo",
-          state: "New York",
-          country: "USA",
-          lat: 42.93627207354969,
-          lng: -78.84793099149458,
-          name: "Martin House",
-          description:
-            "One of Frank Lloyd Wright's most important works, this quintessential Prairie house is now accepting select overnight guests - come take in the views of the magnficent landscape and historic architecture!",
-          price: 8000,
-          previewImage:
-            "https://franklloydwright.org/wp-content/uploads/2021/01/MartinHouse024-1440x640.jpg",
-        },
-        {
-          userId: 3,
-          address: "951 Chicago AVe",
+          address: "951 Chicago Ave",
           city: "Oak Park",
           state: "Illinois",
           country: "USA",
--- backend/db/seeders/20220706043323-demo-image.js
+++ backend/db/seeders/20220706043323-demo-image.js
@@ -88,6 +88,172 @@ module.exports = {
           imageableType: "Spot",
           url: "https://images.squarespace-cdn.com/content/v1/54e65d77e4b06d2d72135b6c/1591383204690-GLZHBSKTNTNGZKSVQXEZ/hanna-house-interior-living-2.jpg",
         },
+        {
+          spotId: 5,
+          imageableType: "Spot",
+          url: "https://cdn.vox-cdn.com/thumbor/htnKeS738au74492utWYN0sEA7o=/0x0:1000x667/1720x0/filters:focal(0x0:1000x667):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/4649661/02_2015_HOLLYHOCK2-86.0.jpg",
+        },
+        {
+          spotId: 5,
+          imageableType: "Spot",
+          url: "https://images.squarespace-cdn.com/content/v1/5e30b4c769dcbf426b421101/1583522251871-D1AJAG8IKPQH9ZB5BU1V/02_2015_HOLLYHOCK2-54.jpg?format=1500w",
+        },
+        {
+          spotId: 5,
+          imageableType: "Spot",
+          url: "https://images.squarespace-cdn.com/content/v1/5e30b4c769dcbf426b421101/1583522252559-0QREAPSHNCBWFZ6CYQO0/02_2015_HOLLYHOCK-145.jpg?format=1500w",
+        },
+        {
+          spotId: 5,
+          imageableType: "Spot",
+          url: "https://images.squarespace-cdn.com/content/v1/5e30b4c769dcbf426b421101/1583522252829-XO4DWAPAFT225UB46TBR/02_2015_HOLLYHOCK-197.jpg?format=1500w",
+        },
+        // SPOT 2 IMAGES
+        {
+          spotId: 6,
+          imageableType: "Spot",
+          url: "https://www.period-homes.com/.image/c_limit%2Ccs_srgb%2Cfl_progressive%2Ch_2000%2Cq_auto:good%2Cw_2000/MTUxMjQxMDQxMDYyNjY4MTEw/4-henrich_conservatory-interior.jpg",
+        },
+        {
+          spotId: 6,
+          imageableType: "Spot",
+          url: "https://www.period-homes.com/.image/c_limit%2Ccs_srgb%2Cfl_progressive%2Ch_2000%2Cq_auto:good%2Cw_2000/MTUxMjQxMDIzNjE0MzYzNDcw/7-henrich_martin-house-reception-room.jpg",
+        },
+        {
+          spotId: 6,
+          imageableType: "Spot",
+          url: "https://www.period-homes.com/.image/c_limit%2Ccs_srgb%2Cfl_progressive%2Ch_2000%2Cq_auto:good%2Cw_2000/MTUxMjQxMDQ0MDE1NDU4MTI2/kc_kratt_interior_3.jpg",
+        },
+        {
+          spotId: 6,
+          imageableType: "Spot",
+          url: "https://martinhouse.org/wp-content/uploads/2021/03/landscaping.jpg",
+        },
+        // SPOT 3 IMAGES
+        {
+          spotId: 7,
+          imageableType: "Spot",
+          url: "https://flwright.org/sites/default/files/detail/homeandstudio_tours_banner.jpg",
+        },
+        {
+          spotId: 7,
+          imageableType: "Spot",
+          url: "https://flwright.org/sites/default/files/detail/homestudiobanner.jpg",
+        },
+        {
+          spotId: 7,
+          imageableType: "Spot",
+          url: "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/26000/26620-Frank-Lloyd-Wright-Home-And-Studio.jpg",
+        },
+        {
+          spotId: 7,
+          imageableType: "Spot",
+          url: "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/26000/26621-Frank-Lloyd-Wright-Home-And-Studio.jpg?impolicy=fcrop&w=1040&h=580&q=mediumHigh",
+        },
+        // SPOT 4 IMAGES
+        {
+          spotId: 8,
+          imageableType: "Spot",
+          url: "https://storage.googleapis.com/clio-images/20548.110376.jpg",
+        },
+        {
+          spotId: 8,
+          imageableType: "Spot",
+          url: "https://storage.googleapis.com/clio-images/20548.42275.jpg",
+        },
+        {
+          spotId: 8,
+          imageableType: "Spot",
+          url: "https://punchmagazine.com/wp-content/uploads/Robert-Siegel-4S0A3551-hanna-house-stanford-300-ppi-05-26-2020-Canon-EOS-7D-Mark-II-scaled.jpg",
+        },
+        {
+          spotId: 8,
+          imageableType: "Spot",
+          url: "https://images.squarespace-cdn.com/content/v1/54e65d77e4b06d2d72135b6c/1591383204690-GLZHBSKTNTNGZKSVQXEZ/hanna-house-interior-living-2.jpg",
+        },
+        {
+          spotId: 9,
+          imageableType: "Spot",
+          url: "https://cdn.vox-cdn.com/thumbor/htnKeS738au74492utWYN0sEA7o=/0x0:1000x667/1720x0/filters:focal(0x0:1000x667):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/4649661/02_2015_HOLLYHOCK2-86.0.jpg",
+        },
+        {
+          spotId: 9,
+          imageableType: "Spot",
+          url: "https://images.squarespace-cdn.com/content/v1/5e30b4c769dcbf426b421101/1583522251871-D1AJAG8IKPQH9ZB5BU1V/02_2015_HOLLYHOCK2-54.jpg?format=1500w",
+        },
+        {
+          spotId: 9,
+          imageableType: "Spot",
+          url: "https://images.squarespace-cdn.com/content/v1/5e30b4c769dcbf426b421101/1583522252559-0QREAPSHNCBWFZ6CYQO0/02_2015_HOLLYHOCK-145.jpg?format=1500w",
+        },
+        {
+          spotId: 9,
+          imageableType: "Spot",
+          url: "https://images.squarespace-cdn.com/content/v1/5e30b4c769dcbf426b421101/1583522252829-XO4DWAPAFT225UB46TBR/02_2015_HOLLYHOCK-197.jpg?format=1500w",
+        },
+        // SPOT 2 IMAGES
+        {
+          spotId: 10,
+          imageableType: "Spot",
+          url: "https://www.period-homes.com/.image/c_limit%2Ccs_srgb%2Cfl_progressive%2Ch_2000%2Cq_auto:good%2Cw_2000/MTUxMjQxMDQxMDYyNjY4MTEw/4-henrich_conservatory-interior.jpg",
+        },
+        {
+          spotId: 10,
+          imageableType: "Spot",
+          url: "https://www.period-homes.com/.image/c_limit%2Ccs_srgb%2Cfl_progressive%2Ch_2000%2Cq_auto:good%2Cw_2000/MTUxMjQxMDIzNjE0MzYzNDcw/7-henrich_martin-house-reception-room.jpg",
+        },
+        {
+          spotId: 10,
+          imageableType: "Spot",
+          url: "https://www.period-homes.com/.image/c_limit%2Ccs_srgb%2Cfl_progressive%2Ch_2000%2Cq_auto:good%2Cw_2000/MTUxMjQxMDQ0MDE1NDU4MTI2/kc_kratt_interior_3.jpg",
+        },
+        {
+          spotId: 10,
+          imageableType: "Spot",
+          url: "https://martinhouse.org/wp-content/uploads/2021/03/landscaping.jpg",
+        },
+        // SPOT 3 IMAGES
+        {
+          spotId: 11,
+          imageableType: "Spot",
+          url: "https://flwright.org/sites/default/files/detail/homeandstudio_tours_banner.jpg",
+        },
+        {
+          spotId: 11,
+          imageableType: "Spot",
+          url: "https://flwright.org/sites/default/files/detail/homestudiobanner.jpg",
+        },
+        {
+          spotId: 11,
+          imageableType: "Spot",
+          url: "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/26000/26620-Frank-Lloyd-Wright-Home-And-Studio.jpg",
+        },
+        {
+          spotId: 11,
+          imageableType: "Spot",
+          url: "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/26000/26621-Frank-Lloyd-Wright-Home-And-Studio.jpg?impolicy=fcrop&w=1040&h=580&q=mediumHigh",
+        },
+        // SPOT 4 IMAGES
+        {
+          spotId: 12,
+          imageableType: "Spot",
+          url: "https://storage.googleapis.com/clio-images/20548.110376.jpg",
+        },
+        {
+          spotId: 12,
+          imageableType: "Spot",
+          url: "https://storage.googleapis.com/clio-images/20548.42275.jpg",
+        },
+        {
+          spotId: 12,
+          imageableType: "Spot",
+          url: "https://punchmagazine.com/wp-content/uploads/Robert-Siegel-4S0A3551-hanna-house-stanford-300-ppi-05-26-2020-Canon-EOS-7D-Mark-II-scaled.jpg",
+        },
+        {
+          spotId: 12,
+          imageableType: "Spot",
+          url: "https://images.squarespace-cdn.com/content/v1/54e65d77e4b06d2d72135b6c/1591383204690-GLZHBSKTNTNGZKSVQXEZ/hanna-house-interior-living-2.jpg",
+        },
         //REVIEW IMAGES
         {
           reviewId: 1,
